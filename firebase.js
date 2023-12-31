d285092450fbebeafcc8b947c13cb5f220dff35f
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2023-12-31 22:30:26 +0500

    working

--- firebase.js
+++ firebase.js
@@ -1,2 +1,31 @@
 
 
+// const analytics = getAnalytics(app);
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import{getAuth,
+    
+
+} from
+'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js'
+
+
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDMSdxBjOMIDIrfszdCq0RIZvWwnGjYcXE",
+    authDomain: "practice-batch-10.firebaseapp.com",
+    projectId: "practice-batch-10",
+    storageBucket: "practice-batch-10.appspot.com",
+    messagingSenderId: "219938660505",
+    appId: "1:219938660505:web:7b3dbbe4bb433e89431808",
+    measurementId: "G-99CV4C9N41"
+};
+
+// Initialize Firebase
+const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+
---
+++ login.js
@@ -0,0 +1,5 @@
+var signupuserName = document.getElementById("signup-userName");
+var signupemail = document.getElementById("signup-email");
+var signuppassword = document.getElementById("signup-password");
+var signupparent = document.getElementById("signup-parent")
+var loginparent = document.getElementById("login-parent")
