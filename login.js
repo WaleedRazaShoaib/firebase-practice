341ffc38ad3ce064ab6c6c3d6c9db61a9032c545
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2024-01-01 23:23:18 +0500

    .

--- login.js
+++ login.js
@@ -2,4 +2,5 @@ var signupuserName = document.getElementById("signup-userName");
 var signupemail = document.getElementById("signup-email");
 var signuppassword = document.getElementById("signup-password");
 var signupparent = document.getElementById("signup-parent")
-var loginparent = document.getElementById("login-parent")
+var loginparent = document.getElementById("login-parent");
+console.log(loginparent)
